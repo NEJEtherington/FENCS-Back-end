4459ca6836205bf3ad4fe395659a7bac6770841c
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-06-20 12:04:56 +0100

    add further categories and images for categories

--- data.sql
+++ data.sql
@@ -7,7 +7,8 @@ CREATE TABLE categories
 (
   topic_id SERIAL PRIMARY KEY,
   slug VARCHAR (50),
-  description VARCHAR(200)
+  description VARCHAR(200),
+  img_url TEXT NOT NULL
 );
 
 CREATE TABLE images
@@ -41,12 +42,28 @@ CREATE TABLE users
 );
 
 INSERT INTO categories
-  (slug, description)
+  (slug, description, img_url)
 VALUES
-  ('shapes', 'Geometry and shapes etc.'),
-  ('objects', 'Assorted objects and tidbits'),
-  ('animals', 'Dogs, cats and so much more'),
-  ('people', 'Assorted members of the human race');
+  ('shapes', 'Geometry and shapes etc.', 'https:
+//images.unsplash.com/photo-1532691900426-90f6909545f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'),
+  ('objects', 'Assorted objects and tidbits', 'https:
+//images.unsplash.com/photo-1560697043-f880bb028f1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80'),
+  ('animals', 'Dogs, cats and so much more', 'https:
+//images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1498&q=80'),
+  ('people', 'Assorted members of the human race', 'https:
+//images.unsplash.com/photo-1454923634634-bd1614719a7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'),
+  ('architecture', 'Buildings and structures', 'https:
+//images.unsplash.com/photo-1487958449943-2429e8be8625?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80' ),
+  ('nature', 'Beautiful landscapes and nature scenes', 'https:
+//images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'),
+  ('transport', 'Planes, trains and automobiles', 'https:
+//images.unsplash.com/photo-1474487548417-781cb71495f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1749&q=80'),
+  ('cities', 'Town and city-scapes', 'https:
+//images.unsplash.com/photo-1498036882173-b41c28a8ba34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'),
+  ('space', 'The cosmos', 'https:
+//images.unsplash.com/photo-1446941611757-91d2c3bd3d45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=866&q=80'),
+  ('tech', 'Gadgets, gizmos and devices', 'https:
+//images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1414&q=80');
 
 INSERT INTO users
   (username, fullname, email_address, date_joined, location, owns_printer, designer_tag, avatar, rating)
