6ac960b1e192c427e6000791f997b43c34a44b2a
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-06-25 16:17:43 +0100

    update data for category images and some image images

--- data.sql
+++ data.sql
@@ -48,26 +48,16 @@ CREATE TABLE users
 INSERT INTO categories
   (slug, description, img_url)
 VALUES
-  ('shapes', 'Geometry and shapes etc.', 'https:
-//images.unsplash.com/photo-1532691900426-90f6909545f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'),
-  ('objects', 'Assorted objects and tidbits', 'https:
-//images.unsplash.com/photo-1560697043-f880bb028f1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80'),
-  ('animals', 'Dogs, cats and so much more', 'https:
-//images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1498&q=80'),
-  ('people', 'Assorted members of the human race', 'https:
-//images.unsplash.com/photo-1454923634634-bd1614719a7b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'),
-  ('architecture', 'Buildings and structures', 'https:
-//images.unsplash.com/photo-1487958449943-2429e8be8625?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80' ),
-  ('nature', 'Beautiful landscapes and nature scenes', 'https:
-//images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'),
-  ('transport', 'Planes, trains and automobiles', 'https:
-//images.unsplash.com/photo-1474487548417-781cb71495f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1749&q=80'),
-  ('cities', 'Town and city-scapes', 'https:
-//images.unsplash.com/photo-1498036882173-b41c28a8ba34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'),
-  ('space', 'The cosmos', 'https:
-//images.unsplash.com/photo-1446941611757-91d2c3bd3d45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=866&q=80'),
-  ('tech', 'Gadgets, gizmos and devices', 'https:
-//images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1414&q=80');
+  ('shapes', 'Geometry and shapes etc.', 'https://dl.dropboxusercontent.com/s/gldwerv64dehp31/shapes.jpeg?dl=0'),
+  ('objects', 'Assorted objects and tidbits', 'https://dl.dropboxusercontent.com/s/vgrmo3iiywiwhnr/objects.jpeg?dl=0'),
+  ('animals', 'Dogs, cats and so much more', 'https://dl.dropboxusercontent.com/s/ksn0rhd6l7u336z/animals.jpeg?dl=0'),
+  ('people', 'Assorted members of the human race', 'https://dl.dropboxusercontent.com/s/ypgvicoooy96p0q/people.jpeg?dl=0'),
+  ('architecture', 'Buildings and structures', 'https://dl.dropboxusercontent.com/s/dnbmz05mroj2y0s/architecture.jpeg?dl=0' ),
+  ('nature', 'Beautiful landscapes and nature scenes', 'https://dl.dropboxusercontent.com/s/uwzz9zvqi1g4f6s/nature.jpeg?dl=0'),
+  ('transport', 'Planes, trains and automobiles', 'https://dl.dropboxusercontent.com/s/0dty45qakmy3vwc/transport.jpeg?dl=0'),
+  ('cities', 'Town and city-scapes', 'https://dl.dropboxusercontent.com/s/0mp8efvucwh5jg0/cities.jpeg?dl=0'),
+  ('space', 'The cosmos', 'https://dl.dropboxusercontent.com/s/meq4j4vbh4eo6g1/space.jpeg?dl=0'),
+  ('tech', 'Gadgets, gizmos and devices', 'https://dl.dropboxusercontent.com/s/zjlfqgzu7akc3hk/tech.jpeg?dl=0');
 
 
 INSERT INTO users
@@ -87,8 +77,8 @@ VALUES
   ('Skull', 'A head bone', 'skull', 'nejetherington', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/ssh77gl7b6afpyh/Screenshot%202019-06-24%2012.14.36.png?dl=0', 'https://dl.dropboxusercontent.com/s/7d4ayvnel7u9ikt/skull.glb?dl=0', 'glb', 'people'),
   ('Cube', 'A simple cube', 'cube', 'lizzyk', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/7h78ork0itwggld/Screenshot%202019-06-24%2012.23.22.png?dl=0', 'https://dl.dropboxusercontent.com/s/kj3xa7aybu8247m/cube.glb?dl=0', 'glb', 'shapes'),
   ('Hammer', 'An old hammer', 'hammer', 'fraserkemp', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/lpnprxfsg1plur1/hammer.png?dl=0', 'https://dl.dropboxusercontent.com/s/s6h7j11rukhzu76/hammer.glb?dl=0', 'glb', 'objects'),
-  ('Penguin', 'A majestic emperor penguin', 'penguin', 'shiva19173', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/1hq5x1uzvpmtu99/emperorpenguin.png?dl=0', 'https://dl.dropboxusercontent.com/s/jr1u917pry6zrro/penguin.glb?dl=0', 'glb', 'animals'),
-  ('Smoking man', 'A man lighting a cigarette', 'smoking man', 'fraserkemp', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/5uprs5sil9p6p6y/Screenshot%202019-06-24%2012.11.25.png?dl=0', 'https://dl.dropboxusercontent.com/s/bca7nxi6vhrf3zj/smokingman.glb?dl=0', 'glb', 'people'),
+  ('Penguin', 'A majestic emperor penguin', 'penguin', 'shiva19173', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/me7lbrncxxaa74m/Screenshot%202019-06-24%2015.39.56.png?dl=0', 'https://dl.dropboxusercontent.com/s/8f7rloczpm8i0m9/penguin%28new%29.glb?dl=0', 'glb', 'animals'),
+  ('Smoking man', 'A man lighting a cigarette', 'smoking man', 'fraserkemp', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/f3ephuupjj1mp29/Screenshot%202019-06-24%2015.20.20.png?dl=0', 'https://dl.dropboxusercontent.com/s/bca7nxi6vhrf3zj/smokingman.glb?dl=0', 'glb', 'people'),
   ('Stones', 'A pile of stones', 'stones', 'shiva19173', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/2egr9bwbsb01b93/freestones.png?dl=0', 'https://dl.dropboxusercontent.com/s/1ysjff7c22kdueg/stones.glb?dl=0', 'glb', 'nature'),
   ('Turtle', 'A snapping turtle laying eggs', 'turtle', 'cpbattrick', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/614eg8heq5lc9cc/Screenshot%202019-06-24%2012.09.41.png?dl=0', 'https://dl.dropboxusercontent.com/s/baagzbeplynbcxd/turtle.glb?dl=0)', 'glb', 'animals'),
   ('Pi', 'Our pi logo!', 'pi', 'nejetherington', '2019-06-13', 0, 'https://dl.dropboxusercontent.com/s/u235fosyc91mov4/pi.png?dl=0', 'https://dl.dropboxusercontent.com/s/dtzx4e0iivesmlq/pi.glb?dl=0', 'glb', 'shapes');
